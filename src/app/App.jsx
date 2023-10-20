2fa9eaf82cfab2e07e31810fe6d7a4a59cb509a0
Hiram Chavez <virtual.liga@gmail.com>
2023-10-20 09:49:04 -0600

    Agregando primer maquetado de la app con Grid

---
+++ src/app/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Header } from "./shared/header/Header.jsx";
+import { Main } from "./shared/main/Main.jsx";
+import { Footer } from "./shared/footer/Footer.jsx";
+
+function App() {
+
+  return (
+    <>
+        <Header />
+        <Main />
+        <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/app/shared/main/Main.jsx
@@ -0,0 +1,7 @@
+import styles from './Main.module.css';
+
+export const Main = () => {
+    return <main className={styles.main}>
+        <p>Aqui va el Main</p>
+    </main>
+}
