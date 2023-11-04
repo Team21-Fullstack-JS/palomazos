5de31cb17faff9586cba61ec3d14c13abda0ffdf
Hiram Chavez <virtual.liga@gmail.com>
2023-11-03 20:23:52 -0600

    Agregando request a MoviesDB

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -4,11 +4,11 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { router } from "./shared/utils/router/router.jsx";
-import { RouterProvider } from "react-router-dom";
+import {router} from "./shared/utils/router/router.jsx";
+import {RouterProvider} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+            <RouterProvider router={router}/>
+    </React.StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/palomazos/',
+    base: '/palomazos/',
   plugins: [react(
       {
         jsxImportSource: "@emotion/react",
