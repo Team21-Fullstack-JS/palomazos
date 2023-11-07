ae57295bbd941bdb3756944c24ef31c176de8208
Hiram Chavez <virtual.liga@gmail.com>
2023-11-07 08:31:33 -0600

    Agregando banner en seccion USER: Mis Peliculas

--- src/app/shared/main/Main.jsx
+++ src/app/shared/main/Main.jsx
@@ -1,10 +1,17 @@
 import styles from './Main.module.css';
 import {Outlet} from "react-router";
+import {theme} from "../utils/theme/theme.jsx";
+import {ThemeProvider} from "@mui/material/styles";
+import {Container} from "@mui/material";
 
 export const Main = () => {
     return (
-        <main className={styles.main}>
-            <Outlet />
-        </main>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="lg" >
+                <main className={styles.main}>
+                    <Outlet />
+                </main>
+            </Container>
+        </ThemeProvider>
     );
 }
