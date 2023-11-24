aad13c4d7701dd446d60042e6fe5b0d1985f8867
marco <marcodzeko@gmail.com>
2023-11-23 17:55:13 -0700

    Landing page actualizada

--- src/app/shared/main/components/landingPage/LandingPage.jsx
+++ src/app/shared/main/components/landingPage/LandingPage.jsx
@@ -1,15 +1,30 @@
 import { Box } from "@mui/material";
 
-export const LandingPage = () => {
+
+export const LandingPage = ({bgImage}) => {
 
     return (
+        <>
         <Box sx={{
             display: 'flex',
             justifyContent: 'center',
             fontSize: '1.5rem',
             paddingTop: '2rem'
         }}>
-            Esta es la Landing Page!
+        
+            BIENVENIDOS A PALOMAZOS
         </Box>
+        <Box sx={{
+            display: 'center',
+            justifyContent: 'center',
+            fontSize: '1rem',
+            paddingTop: '2rem',
+            width: '65rem',
+        }}>
+            Palomazos es un sitio donde los usuarios podrán compartir sus reseñas, comentar opiniones y compartir su gusto por las películas tanto clásicas como actuales. Permitiendo generar una comunidad cinéfila que permita a todos descubrir nuevas obras del septimo arte.
+        </Box>
+        </>
     )
+    
 }
+
