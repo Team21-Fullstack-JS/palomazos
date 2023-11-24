2f8e58296ab9fff1442c60f6817bb3f4f39b09dc
Hiram Chavez <virtual.liga@gmail.com>
2023-11-24 12:31:15 -0600

    Mejorando el buscador: muestra mas paginas con el mismo resultado

--- src/app/Movies/components/buttonMoreMovies/ButtonMoreMovies.jsx
+++ src/app/Movies/components/buttonMoreMovies/ButtonMoreMovies.jsx
@@ -5,28 +5,50 @@ import {GetMoviesFromMovieDB} from "../../../shared/utils/router/loaders/GetMovi
 
 export const ButtonMoreMovies = (props) => {
 
-    const { setArrayMovies, page, setPage, totalPages, path } = props;
+    const { setArrayMovies, page, setPage, totalPages, path, fetchSearchMovies, stringSearch } = props;
 
     const [disabledButton, setDisabledButton] = useState(false);
+    const [message, setMessage] = useState('Mostrar más');
 
     const handleClick = () => {
+
+        setDisabledButton(true);
+
+        function pairArray(res) {
+            setArrayMovies((prev) => {
+                const combinedArray = [...prev, ...res.results];
+                return Array.from(
+                    new Set(combinedArray.map(obj => obj.id)))
+                    .map(id => combinedArray.find(obj => obj.id === id));
+
+            });
+        }
+
         if (page < totalPages) {
-            setDisabledButton(true);
 
-            GetMoviesFromMovieDB(path, 'es-US', page + 1)
+            let data = null;
+
+            if (stringSearch) {
+                data = fetchSearchMovies(stringSearch, page + 1);
+            } else {
+                data = GetMoviesFromMovieDB(path, 'es-US', page + 1);
+            }
+
+            data
                 .then((res) => {
-                    setArrayMovies((prev) => {
-                        const combinedArray = [...prev, ...res.results];
-                        return Array.from(
-                            new Set(combinedArray.map(obj => obj.id)))
-                            .map(id => combinedArray.find(obj => obj.id === id));
-
-                    });
-                    setDisabledButton(false);
+                    pairArray(res);
                 })
                 .catch((err) => console.log(err));
 
             setPage(page + 1);
+        }
+
+        if (page === totalPages) {
+            setDisabledButton(true);
+            setMessage('No hay más resultados');
+        } else {
+            setDisabledButton(false);
+            setMessage('Mostrar más');
         }
     }
 
@@ -38,7 +60,7 @@ export const ButtonMoreMovies = (props) => {
             onClick={handleClick}
             disabled={disabledButton}
         >
-            Mostrar más
+            { message }
         </Button>
     )
 }
