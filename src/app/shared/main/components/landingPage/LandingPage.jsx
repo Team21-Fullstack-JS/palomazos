4225054305d8e5134b2689d77b27163b8426b304
Carlos Chirinos <carchirinosg@gmail.com>
2023-11-25 21:56:54 -0600

    Terminando landing page

--- src/app/shared/main/components/landingPage/LandingPage.jsx
+++ src/app/shared/main/components/landingPage/LandingPage.jsx
@@ -1,25 +1,62 @@
+import {Box, Typography, Button} from "@mui/material";
+
+
 
 export const LandingPage = () => {
 
+    const handleClick = () => alert("clicked");
+
     return (
-        <>
-        <div
-        style={{
-          backgroundImage: `url('../../../../../../../palomazos/assets/movies-home.jpeg')`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          height: '80vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        </div>
-        <h1 style={{ color: 'black', fontSize: '3rem', textAlign: 'center' }}>¡Bienvenido a Palomazos!</h1>
+      <div
+      style={{
+        backgroundImage: `url('../../../../../../../palomazos/assets/fondoo-collage.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            fontSize: '1.5rem',
+            paddingTop: '6rem',
+            color: 'white'
+        }}>
+            <Typography
+            variant="h5"
+            > 
+              ¡BIENVENIDOS A PALOMAZOS!
+            </Typography>
+        </Box>
+        <Box sx={{
+            display: 'flex',
+            textAlign: 'center',
+            fontSize: '1rem',
 
-        </>
-
-    )
-    
+            padding: '4rem 6rem',
+            color: 'white'
+        }}>
+            <Typography>
+              Palomazos es un sitio donde los usuarios podrán compartir sus reseñas, comentar opiniones y compartir su gusto por las películas tanto clásicas como actuales. Permitiendo generar una comunidad cinéfila que permita a todos descubrir nuevas obras del septimo arte.
+            </Typography>
+        </Box>
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            paddingBottom: '2rem'
+        }}>
+        <Button 
+            onClick={handleClick}     
+            variant="contained" 
+            color="primary"
+            >
+                Comenzar
+            </Button>
+        </Box>
+      </div>
+    )    
 }
 
